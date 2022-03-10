e771a894a4e3218f2946678891555a49d1bb7e5f
Zhiyuan Ma <631253076@qq.com>
2022-03-09 23:36:58 -0500

    add cvs generate and download

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const fs = require('fs');
 const {Client, Intents, Collection} = require('discord.js');
-const {token} = require('./config.json');
+const {token, mongodb} = require('./config.json');
+const mongoose = require("mongoose");
 
+require('./models/PiazzaData')
+mongoose.connect(mongodb).then(() => console.log('MongoDB Connected')).catch(err => console.log(err));
 
 const client = new Client({intents: [Intents.FLAGS.GUILDS]});
 
@@ -18,7 +21,7 @@ const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'
 
 eventFiles.forEach(file => {
     const event = require(`./events/${file}`);
-    if(event.once) {
+    if (event.once) {
         client.once(event.name, (...args) => event.execute(...args));
     } else {
         client.on(event.name, (...args) => event.execute(...args));

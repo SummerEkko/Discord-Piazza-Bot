38b61966ed91d6a1f3506a38b09a6d1c8f0c2146
Zhiyuan Ma <631253076@qq.com>
2022-03-11 00:17:17 -0500

    modified test

--- test/test.js
+++ test/test.js
@@ -9,7 +9,6 @@ const optIn = require('../commands/optIn');
 const optOut = require('../commands/optOut');
 const set = require('../commands/set');
 const user = require('../commands/user');
-const index = require('../index');
 const deployCommands = require('../deploy-commands');
 const executeBotPy = require('../utils/executeBotPy');
 
@@ -20,7 +19,7 @@ const exportCSV = require('../utils/exportCSV')
 describe("exportCSV test", function () {
     this.timeout(5000);
 
-    it("should create file",  function (   ) {
+    it("should create file", function () {
         console.log(process.cwd())
 
         fs.exists('piazza.csv', (exists) => {
@@ -48,12 +47,12 @@ class interaction {
     constructor() {
         this.options = {
             getInteger(name) {
-                const m = { 'p1': "1", 'p2': "2", 'p3': "3", 'p4': "4" }
+                const m = {'p1': "1", 'p2': "2", 'p3': "3", 'p4': "4"}
                 return m[name];
             }
         }
 
-        this.user = { tag: "abc", id: "def", username: "ABC" }
+        this.user = {tag: "abc", id: "def", username: "ABC"}
         this.member = {
             client: "123", displayName: "Name", guild: "012",
             permissions: "ADMINISTRATOR", user: "User", roles: "Admin",
@@ -68,32 +67,12 @@ class interaction {
     }
 }
 
-require('../commands/downloadCSV');
-require('../commands/login');
 const setTest = require('../commands/set');
-
 
 
 describe("Set Tests", function () {
     it("should return the correct weight setting", function () {
-        const i = new interaction();
-        setTest.execute(i);
-describe("WeatherBot Tests", function () {
 
-    // this.timeout(5000);
-    // it("login test", function() {
-    //     const i = new interaction();
-    //     login.execute(i);
-    //     assert.equal(i.c, "This command can only be used in a server");
-    // });
-
-    // it("csv test", async function() {
-    //     const i = new interaction();
-    //     let result = await downloadCSV.execute(i);
-    //     assert.equal(i.c.content, "CSV file generated.");
-    // });
-
-    it("should return the correct weight setting", function () {
         const i = new interaction();
         set.execute(i);
         weightSetting = `Parameters:\n` +
@@ -111,5 +90,4 @@ describe("WeatherBot Tests", function () {
             + `guild: 012\npermissions: "ADMINISTRATOR"\nuser: User\ndisplayAvatarURL(): google.com\n`;
         assert.equal(i.c, userStr);
     })
-
 });

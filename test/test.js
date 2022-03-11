63799df4c3f7335fc9367eb348ee1c5289fc520b
Rachel Chen <rschen@ncsu.edu>
2022-03-10 23:42:55 -0500

    Add user test

---
+++ test/test.js
@@ -0,0 +1,74 @@
+var chai = require('chai');
+var assert = chai.assert,
+    expect = chai.expect;
+process.env.NODE_ENV = 'test';
+
+const downloadCSV = require('../commands/downloadCSV');
+const login = require('../commands/login');
+const optIn = require('../commands/optIn');
+const optOut = require('../commands/optOut');
+const set = require('../commands/set');
+const user = require('../commands/user');
+const index = require('../index');
+const deployCommands = require('../deploy-commands');
+const executeBotPy = require('../utils/executeBotPy');
+
+class interaction {
+    constructor() {
+        this.options = {
+            getInteger(name) {
+                const m = { 'p1': "1", 'p2': "2", 'p3': "3", 'p4': "4" }
+                return m[name];
+            }
+        }
+
+        this.user = { tag: "abc", id: "def", username: "ABC" }
+        this.member = {
+            client: "123", displayName: "Name", guild: "012",
+            permissions: "ADMINISTRATOR", user: "User", roles: "Admin",
+            displayAvatarURL() {
+                return "google.com";
+            }
+        }
+    }
+
+    async reply(options) {
+        this.c = options;
+    }
+}
+
+describe("WeatherBot Tests", function () {
+
+    // this.timeout(5000);
+    // it("login test", function() {
+    //     const i = new interaction();
+    //     login.execute(i);
+    //     assert.equal(i.c, "This command can only be used in a server");
+    // });
+
+    // it("csv test", async function() {
+    //     const i = new interaction();
+    //     let result = await downloadCSV.execute(i);
+    //     assert.equal(i.c.content, "CSV file generated.");
+    // });
+
+    it("should return the correct weight setting", function () {
+        const i = new interaction();
+        set.execute(i);
+        weightSetting = `Parameters:\n` +
+            `Questions asked: 1\n` +
+            `Answers to questions: 2\n` +
+            `Most views: 3\n` +
+            `Endorsement by other users: 4`
+        assert.equal(i.c, weightSetting)
+    })
+
+    it("should return the correct user info", function () {
+        const i = new interaction();
+        user.execute(i);
+        userStr = `Your tag: abc\nYour id: def\nYour username: ABC\nclient: "123"\n\ndisplayName: Name\n`
+            + `guild: 012\npermissions: "ADMINISTRATOR"\nuser: User\ndisplayAvatarURL(): google.com\n`;
+        assert.equal(i.c, userStr);
+    })
+
+});

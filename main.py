b0ee721df0a0bd467002c3b6ec999782b51acdf7
Rachel Chen <rschen@ncsu.edu>
2022-03-30 19:12:06 -0400

    Fix daily data update

--- main.py
+++ main.py
@@ -13,33 +13,32 @@ username = instructorTable.find_one()['InstructorID']
 password = instructorTable.find_one()['InstructorPassword']
 networkID = instructorTable.find_one()['NetworkID']
 
-def initTotal():
+def updateTotal():
+    """
+    Overwrite total data database with latest data
+    """
     totalData.delete_many({})
     allData = ps.pull_data(username, password, networkID)
     totalData.insert_many(allData)
 
 def updateDaily():
-    allData = ps.pull_one_day_data(username, password, networkID)
-    dailyData.insert_many(allData)
-
-def updateTotal():
-    lastDay = ps.pull_one_day_data(username, password, networkID)
-    if len(lastDay) > 0:
-        for student in lastDay:
-            changed = {item[0]: item[1] for item in student.items() if item[1] != 0 and item[0] != 'Email'}
-            totalData.update_one({'Email': student['Email']}, {'$inc': changed})
-
-def empty():
+    """
+    Overwrite daily data database with latest data
+    """
     dailyData.delete_many({})
+    allData = ps.pull_data(username, password, networkID)
+    for student in allData:
+        obs = totalData.find_one({'Email': student['Email']})
+        for key in student.keys():
+            if key != 'Email':
+                student[key] -= obs[key]
+    dailyData.insert_many(allData)
 
 sched = BlockingScheduler()
 
 def scheduleJobs():
-    sched.add_job(initTotal, 'cron', hour='21', minute='20', second='00')
+    sched.add_job(updateTotal, 'cron', hour='21', minute='20', second='00')
     sched.add_job(updateDaily, 'cron', hour='21', minute='35', second='00')
-    # empty the daily database
-    sched.add_job(empty, 'cron', hour='00', minute='00', second='00')
-    # sched.add_job(empty, 'interval', seconds=5)
     sched.start()
 
 if __name__ == '__main__':

0bba9469fa28748d051bd144cd3c302cbf2d5b63
Zhiyuan Ma <631253076@qq.com>
2022-03-31 20:55:45 -0400

    refactor based on zijun code

--- utils/sendPrivateMessage.js
+++
@@ -1,9 +0,0 @@
-const { Client, Intents, Collection } = require("discord.js");
-const { token, mongodb } = require("../config.json");
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-
-client.users.fetch("802398350761525268", false).then((user) => {
-  user.send("private message!");
-});
-
-client.login(token);
